cc4cf540bc476b0f6b79ebbfa5bb0267b0da81ae
ShaundaK <sdking1913@gmail.com>
2018-11-20 12:35:01 -0600

    api connects to js

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div v-for="place in places">
+      <h3>{{ place.name }}</h3>
+      <h4>{{ place.address }}</h4>
+    </div>
   </div>
 </template>
 
@@ -8,13 +12,22 @@
 </style>
 
 <script>
+  var axios = require('axios');
 export default {
   data: function() {
     return {
-      message: "Welcome to Vue.js!"
+      message: "Welcome to Vue.js!",
+      places: []
     };
   },
-  created: function() {},
+  created: function() {
+    console.log('in the created function');
+    console.log(this)
+    axios.get('http://localhost:3000/api/places').then(function(response) {
+      console.log(response.data);
+      this.places = response.data;
+    }.bind(this))
+  },
   methods: {},
   computed: {}
 };
